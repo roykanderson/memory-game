5c688f43d7bfaf1d1921532d2606080d484f642c
Roy Anderson <andersro@oregonstate.edu>
2021-01-09 08:40:13 -0800

    Add getRandomSubset and populateTiles

--- memoryGame.js
+++ memoryGame.js
@@ -1,35 +1,50 @@
-let imgSources = [ {src: "img/lion.jpg", use: 0},
-                   {src: "img/leopard.jpg", use: 0},
-                   {src: "img/tiger.jpg", use: 0},
-                   {src: "img/koala.png", use: 0},
-                   {src: "img/zebra.jpg", use: 0},
-                   {src: "img/giraffe.jpg", use: 0},
-                   {src: "img/elephant.jpg", use: 0},
-                   {src: "img/hippo.jpg", use: 0}
-                 ];
+function ImgSrc(imgSrcObject) {
+    this.imgSrc = imgSrcObject;
+    this.length = this.imgSrc.length;
+
+    this.getElement = function(index) {
+        return this.imgSrc[index];
+    };
+
+    this.getRandomSubset = function(subsetLength) {
+        if (subsetLength <= this.length) {
+            let subset = [];
+            while (subset.length < subsetLength) {
+                let rand = Math.floor(Math.random() * this.length);
+                if (subset.indexOf(this.getElement(rand)) === -1) {
+                    subset.push(this.getElement(rand));
+                }
+            }
+            return subset;
+        }
+    };
+}
 
 // Board constructor. dimensions: number of rows/cols
 function Board(dimensions) {
-    this.tiles = this.populateTiles(dimensions);
 
-    this.populateTiles = function(dimensions) {
+    this.populateTiles = function(dim) {
+        let subset = src.getRandomSubset((dim*dim)/2);
         let tiles = [];
-        for (let i = 0; i < dimensions; i++) {
-            let random = getRandomNumber(dimensions);
+        for (let i = 0; i < dim*dim; i++) {
+            let rand = Math.floor(Math.random() * subset.length);
+            tiles.push(new Tile(subset[rand].src));
+            subset[rand].use++;
+            if (subset[rand].use > 1) {
+                subset.splice(rand, 1);
+            }
         }
+        console.log(subset);
+        return tiles;
     };
+
+    // this.tiles = this.populateTiles(dimensions);
 }
 
 // Tile constructor. src: file path for the image in the tile
 function Tile(src) {
     this.src = src;
     this.matched = false;
-}
-
-function getRandomNumber(dimensions) {
-    let n = (dimensions * dimensions) / 2;
-    let random = Math.floor(Math.random * n);
-    return random;
 }
 
 function getGridTemplate(n) {
@@ -49,3 +64,15 @@ function setGridTemplate(dimensions) {
     gameContainer.style.gridTemplateColumns = gridTemplate;
     gameContainer.style.gridTemplateRows = gridTemplate;
 }
+
+let src = new ImgSrc([
+    {src: "img/lion.png", use: 0},
+    {src: "img/leopard.png", use: 0},
+    {src: "img/tiger.png", use: 0},
+    {src: "img/koala.png", use: 0},
+    {src: "img/zebra.png", use: 0},
+    {src: "img/giraffe.png", use: 0},
+    {src: "img/elephant.png", use: 0},
+    {src: "img/hippo.png", use: 0}
+]);
+let board = new Board(4);
